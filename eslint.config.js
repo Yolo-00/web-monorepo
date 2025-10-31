ab56cf74831047a67c4ca49c24ff42acccb2e815
郭某人 <129492349+Yolo-00@users.noreply.github.com>
2025-10-31 17:53:28 +0800

    feat: 增加提交拦截

--- eslint.config.js
+++ eslint.config.js
@@ -9,37 +9,37 @@ import globals from "globals";
 const ignores = ["node_modules", "dist", "**/node_modules", "**/dist"];
 
 export default defineConfig({
-  ignores,
-  plugins: {
-    "@typescript-eslint": typescriptPlugin,
-    prettier: prettierPlugin
-  },
-  files: ["**/*.{ts,tsx,js,jsx,mjs,cjs}"],
-  languageOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    parser: tseslint.parser, // 已经是 typescript-eslint 自带 parser
-    globals: {
-      ...globals.browser
-    }
-  },
-  rules: {
-    // 展开推荐规则
-    ...js.configs.recommended.rules,
-    ...tseslint.configs.recommended[0].rules, // 注意：tseslint.configs.recommended 是数组
-    ...typescriptPlugin.configs.recommended.rules,
-    ...eslintConfigPrettier.rules,
+	ignores,
+	plugins: {
+		"@typescript-eslint": typescriptPlugin,
+		prettier: prettierPlugin
+	},
+	files: ["**/*.{ts,tsx,js,jsx,mjs,cjs}"],
+	languageOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		parser: tseslint.parser, // 已经是 typescript-eslint 自带 parser
+		globals: {
+			...globals.browser
+		}
+	},
+	rules: {
+		// 展开推荐规则
+		...js.configs.recommended.rules,
+		...tseslint.configs.recommended[0].rules, // 注意：tseslint.configs.recommended 是数组
+		...typescriptPlugin.configs.recommended.rules,
+		...eslintConfigPrettier.rules,
 
-    // 自定义规则
-    "no-var": "error",
-    "no-multiple-empty-lines": ["error", { max: 1 }],
-    "prefer-const": "warn",
-    "no-use-before-define": "warn",
+		// 自定义规则
+		"no-var": "error",
+		"no-multiple-empty-lines": ["error", { max: 1 }],
+		"prefer-const": "warn",
+		"no-use-before-define": "warn",
 
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" }
-    ],
-    "@typescript-eslint/no-explicit-any": "warn"
-  }
+		"@typescript-eslint/no-unused-vars": [
+			"error",
+			{ argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" }
+		],
+		"@typescript-eslint/no-explicit-any": "warn"
+	}
 });
